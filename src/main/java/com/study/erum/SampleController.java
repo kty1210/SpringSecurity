721c5a5f53bf3545e7938dac3b40e10834d958ed
kty <kwonabraham@naver.com>
2024-03-18 15:37:53 +0900

    all member admin 페이지 구현

--- src/main/java/com/study/erum/SampleController.java
+++ src/main/java/com/study/erum/SampleController.java
@@ -15,4 +15,12 @@ public class SampleController {
   public void doAll() {
     log.info("do all can access everybody");
   }
+  @GetMapping("/member")
+  public void doMember() {
+    log.info("logined member");
+  }
+  @GetMapping("/admin")
+  public void doAdmin() {
+    log.info("admin only");
+  }
 }
