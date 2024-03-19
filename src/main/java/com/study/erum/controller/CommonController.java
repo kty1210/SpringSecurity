300cd4a4db9866a07f39de6e604b4254fe134357
kty <kwonabraham@naver.com>
2024-03-19 10:35:00 +0900

    로그인 화면 구현

--- src/main/java/com/study/erum/controller/CommonController.java
+++ src/main/java/com/study/erum/controller/CommonController.java
@@ -17,4 +17,19 @@ public class CommonController {
     
     model.addAttribute("msg", "Acess Denied");
   }
+  
+  @GetMapping("/customLogin")
+  public void loginInput(String error, String logout, Model model) {
+    log.info("error : " + error);   
+    log.info("logout : " + logout);   
+    
+    if(error != null) {
+      model.addAttribute("error", "Login Error Check Your Account");
+    }
+    
+    if(logout != null) {
+      model.addAttribute("logout", "Logout!");
+    }
+    
+  }
 }
