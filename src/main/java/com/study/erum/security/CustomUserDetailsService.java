8233fd0b94ddb8598bb5f8b558c9587898c1bba6
kty <kwonabraham@naver.com>
2024-03-19 16:39:35 +0900

    CustomUserDetailsService 클래스 생성

--- src/main/java/com/study/erum/security/CustomUserDetailsService.java
+++ src/main/java/com/study/erum/security/CustomUserDetailsService.java
@@ -5,21 +5,26 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+import com.study.erum.domain.CustomUser;
+import com.study.erum.domain.MemberVO;
 import com.study.erum.mapper.MemberMapper;
 
+import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 
 @Log4j
 public class CustomUserDetailsService implements UserDetailsService {
 
-  @Autowired
+  @Setter(onMethod_ = { @Autowired })
   private MemberMapper memberMapper;
-
   @Override
-  public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
-    
+  public UserDetails loadUserByUsername(String userName)
+                              throws UsernameNotFoundException {
+
     log.warn("Load User By UserName : " + userName);
-    
-    return null;
-  }
+    // userName means userid
+    MemberVO vo = memberMapper.read(userName);
+    log.warn("queried by member mapper: " + vo);
+    return vo == null ? null : new CustomUser(vo);
+  } 
 }
