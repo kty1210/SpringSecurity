21109e3b9562222e4a13eb7539a4c795909735c3
kty <kwonabraham@naver.com>
2024-03-19 15:31:50 +0900

    MemberVO AuthVO 생

---
+++ src/main/java/com/study/erum/domain/AuthVO.java
@@ -0,0 +1,5 @@
+package com.study.erum.domain;
+
+public class AuthVO {
+
+}
---
+++ src/main/java/com/study/erum/domain/MemberVO.java
@@ -0,0 +1,14 @@
+package com.study.erum.domain;
+
+public class MemberVO {
+
+  private String userid;
+  private String userpw;
+  private String userName;
+  private boolean enabled;
+  
+  private String userid;
+  private String userid;
+  private String userid;
+  
+}
